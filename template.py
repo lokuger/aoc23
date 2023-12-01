88e429226e25036ba058a05966701a2fc83bc64d
Lorenz Kuger <57869635+lokuger@users.noreply.github.com>
2023-12-01 13:45:19 +0100

    add template file

---
+++ template.py
@@ -0,0 +1,15 @@
+def main():
+    with open('input<i>.txt') as f:
+        input = f.readlines()
+    
+    part1(input)
+    part2(input)
+
+def part1(input):
+    pass
+
+def part2(input):
+    pass
+
+if __name__ == '__main__':
+    main()
