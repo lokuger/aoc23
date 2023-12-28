0f5d5dbbcaf24ec9e3a019f6f191cf8586e14c75
Lorenz Kuger <57869635+lokuger@users.noreply.github.com>
2023-12-28 12:23:18 +0100

    day 25 proper soln with kargers algo instead of graphviz cheating

--- day25.py
+++ day25.py
@@ -1,4 +1,6 @@
 import re
+import numpy as np
+from numpy.random import choice
 
 def main():
     with open('input25cut.txt') as f:
@@ -17,10 +19,7 @@ def bfs_count_component(G,start):
             if v not in V: Q.append(v)
     return len(V)
 
-def part1(input):
-    #with open('output25.txt','w') as f:
-    #    f.write('\n'.join([line.split(':')[0] + ' -- ' + ','.join(line.split(':')[1].strip().split()) for line in input]))
-
+def construct_graph(input):
     G = {}
     for line in input:
         u,V = line.split(':')[0],line.split(':')[1].strip().split()
@@ -31,10 +30,56 @@ def part1(input):
                 G[v].append(u)
             else:
                 G[v] = [u]
-    
-    return bfs_count_component(G,'vfx') * bfs_count_component(G,'bgl')
-            
+    return G
 
+######## soln using graph viz
+# def part1(input):
+#     with open('output25.txt','w') as f:
+#         f.write('\n'.join([line.split(':')[0] + ' -- ' + ','.join(line.split(':')[1].strip().split()) for line in input]))
+#     # now delete 3 edges manually, saved input25cut.txt and run again on that input file
+#     G = construct_graph(input)
+#     return bfs_count_component(G,'vfx') * bfs_count_component(G,'bgl')
+
+def karger(G):
+    N = [u for u in G]
+    W = [len(G[u]) for u in G]
+    nE = sum(W)
+    while len(N) > 2:
+        #print('Current graph: {}'.format(G))
+        # draw random edge
+        iu = choice(len(N),p=np.array(W)/nE)
+        u = N[iu]
+        tu = G.pop(u)
+        v = tu.pop(choice(len(tu)))
+        N.pop(iu)
+        iv = N.index(v)
+        tv = G.pop(v)
+        N.pop(iv)
+        tv.remove(u)
+        #print('Collapse {} -- {}'.format(u,v))
+        # collapse edge
+        uv = u+v
+        tuv = [w for w in tu+tv if w not in [u,v]]
+        n_cllps = 2+sum([1 if w in [u,v] else 0 for w in tu+tv])
+        #print('Neighbors of collapsed nodes: {}'.format(tuv))
+        N.append(uv)
+        nu = W.pop(iu)
+        nv = W.pop(iv)
+        W.append(nu+nv-n_cllps)
+        nE -= n_cllps
+        G[uv] = tuv
+        # replace u and v by uv in neighboring nodes of both
+        for w in G[uv]:
+            G[w] = [uv if x in [u,v] else x for x in G[w]]
+    return int(len(N[0])/3), int(len(N[1])/3)
+
+
+def part1(input):
+    G = construct_graph(input)
+    a,b = karger(G)
+    print(a,b,a*b)
+    #for c in C: G[c[0]].remove(c[1])
+    #return bfs_count_component(G,C[0][0]) * bfs_count_component(G,C[0][1])
 
 def part2(input):
     pass
